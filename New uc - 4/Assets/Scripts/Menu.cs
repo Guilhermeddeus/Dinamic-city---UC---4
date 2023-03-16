35f608e6c653051f2b5a05f8ca55dc3c37916c46
Guilhermeddeus <guilhermebarrosddeus@gmail.com>
2023-03-15 21:51:24 -0400

    menu commit

---
+++ New uc - 4/Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    public void LoadScene(int indexScene)
+    {
+        SceneManager.LoadScene(indexScene);
+    } 
+    
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Você saiu");
+    }
+
+    public void OpenOption()
+    {
+
+    }
+
+    public void CloseOption()
+    {
+
+    }
+
+   
+}
