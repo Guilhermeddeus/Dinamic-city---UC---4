61a21d308a026eab4ed658900548d89407428c25
Kanhamolax <gui.bianchini@gmail.com>
2023-04-03 04:09:15 -0400

    Paralax, e melhoria do cenario

---
+++ New uc - 4/Assets/Scripts/ScriptsGame/Paralax.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    public GameObject objectMove,endLine,originalPosition;
+    public float speed;
+    float altura;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        altura=objectMove.transform.position.y;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objectMove.transform.Translate( -speed * Time.deltaTime, 0,0);
+        if(objectMove.transform.position.x<=endLine.transform.position.x) 
+        {
+             objectMove.transform.position = new Vector2(originalPosition.transform.position.x,altura);
+           
+        }
+    }
+}
