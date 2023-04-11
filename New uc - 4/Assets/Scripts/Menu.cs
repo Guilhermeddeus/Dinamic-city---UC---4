5c36b85ead33932c4e5dea7a867225b8ffaeeffd
Guilhermeddeus <guilhermebarrosddeus@gmail.com>
2023-04-10 21:51:39 -0400

    Menu atualizado
    coloquei algumas coisas novas como opções, mas não está concluida

--- New uc - 4/Assets/Scripts/Menu.cs
+++ New uc - 4/Assets/Scripts/Menu.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
+
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoes;
     public void LoadScene(int indexScene)
     {
         SceneManager.LoadScene(indexScene);
-    } 
-    
+    }
+
     public void ExitGame()
     {
         Application.Quit();
@@ -17,13 +20,15 @@ public class Menu : MonoBehaviour
 
     public void OpenOption()
     {
-
+        painelMenuInicial.SetActive(false);
+        painelOpcoes.SetActive(true);
     }
 
     public void CloseOption()
     {
-
+        painelOpcoes.SetActive(false);
+        painelMenuInicial.SetActive(true);
     }
 
-   
+
 }
