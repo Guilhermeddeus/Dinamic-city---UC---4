a9029cc7b22923668f03a74625ee253c373b6b4f
filipin04 <123595764+filipin04@users.noreply.github.com>
2023-04-18 23:52:10 -0400

    comentando o codigo

--- New uc - 4/Assets/Scripts/Menu.cs
+++ New uc - 4/Assets/Scripts/Menu.cs
@@ -9,27 +9,27 @@ public class Menu : MonoBehaviour
     [SerializeField] private GameObject painelOpcoes;
     public void LoadScene(int indexScene)
     {
-        SceneManager.LoadScene(indexScene);
+        SceneManager.LoadScene(indexScene);// para rodar a cena do game
     }
 
     public void ExitGame()
     {
-        Application.Quit();
+        Application.Quit();// apertar para sair 
         Debug.Log("Você saiu");
     }
 
     public void OpenOption()
     {
         Debug.Log("Você Entrou nas opções");
-        painelMenuInicial.SetActive(false);
-        painelOpcoes.SetActive(true);
+        painelMenuInicial.SetActive(false);// ao apertar no botão de opções desativar a cena de menu
+        painelOpcoes.SetActive(true);// ativer a cena de opções
     }
 
     public void CloseOption()
     {
         Debug.Log("Você saiu das opções");
-        painelOpcoes.SetActive(false);
-        painelMenuInicial.SetActive(true);
+        painelOpcoes.SetActive(false); // ao apertar o botão de voltar, desativa a cena de opções
+        painelMenuInicial.SetActive(true);// ativa a cena de menu novamente 
     }
 
 
